b4fdcfde14edf42b5d0a03661e5b42f63bc0002d
ww2497 <ww2497@rit.edu>
2021-02-20 22:43:56 -0500

    Basic detection of cart items + logic cleanup

--- content.js
+++ content.js
@@ -2,33 +2,34 @@ console.log("Content injected.");
 
 chrome.runtime.onMessage.addListener(
 	function (message, sender, response) {
-		var loading = document.getElementsByClassName("load-app");
-		if (loading.length != 0)
-			return null;
-
-		console.log(message);
 		console.log(message.type);
 
+		var loading = document.getElementsByClassName("load-app");
+		if (loading.length != 0) {
+			console.log("Wait for loading!");
+			response(null);
+			return
+		}
+
+		var table = [];
 		switch (message.type) {
 			case 'order':
-				var table = [];
 				var items = document.getElementsByClassName("item-name");
-				for (item of items) {
+				for (item of items)
 					table.push(item.innerText);
-				}
-				response(table);
 				break
 			case 'cart':
-				var items = document.getElementsByClassName("side-cart-content");
-				break
 			case 'side-cart':
-				var items = document.getElementsByClassName("side-cart-content");
-				response(null);
+				var items = document.getElementsByClassName("name");
+				for (item of items)
+					if (item.innerText != "")
+						table.push(item.innerText);
 				break
 			default:
 				console.log("Unknown message received.");
-				response(null);
 				break
 		}
+		console.log(table);
+		response(table);
 	}
 )
